86ce74ef7543a48be1a6c43efef48585bc6ca8b2
John Shin <johnshin62@gmail.com>
2024-04-08 22:15:11 -0400

    better code

--- backtester.py
+++
@@ -1,8 +0,0 @@
-#accept any n amount of csv files, and parse them to simulate TradeState
-#differentiate between prices_round_x_day_y and trades_round_x_day_y
-#simulate orderbook based on trades csv
-#simulate stock prices based on prices csv
-
-#at every timestamp on each day execute orders based on trades and return total PnL from 
-
-#could possibly abandon due to time constraints
